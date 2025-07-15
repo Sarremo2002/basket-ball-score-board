d3a61bd9def1cb080dc71b650e2c62d7f7c6b98e
Sarremo2002 <selimwaleed2@icloud.com>
2025-07-15 16:26:01 +0300

    added home score button adding functionality

---
+++ index.js
@@ -0,0 +1,18 @@
+let homeSCORE = document.getElementById("home-score");
+
+let count = 0;
+
+function add1() {
+  count += 1;
+  homeSCORE.textContent = count;
+}
+
+function add2() {
+  count += 2;
+  homeSCORE.textContent = count;
+}
+
+function add3() {
+  count += 3;
+  homeSCORE.textContent = count;
+}
